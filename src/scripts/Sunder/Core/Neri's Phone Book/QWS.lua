1e64617ea0ae2b7535a2c0007dad8e03232a45b1
TheWanderingCrow <contact@wanderingcrow.net>
2025-03-09 16:31:37 -0400

    lirou npb fixes

--- src/scripts/Sunder/Core/Neri's Phone Book/QWS.lua
+++ src/scripts/Sunder/Core/Neri's Phone Book/QWS.lua
@@ -3,18 +3,22 @@ function npb.getOnlineCharacters()
 end
 
 function npb.handleResponse(_, url, body)
-  if url == "https://api.aetolia.com/characters.json" then
+  if url == "https://api.aetolia.com/characters.json" or url == "http://api.aetolia.com/characters.json" then
     local data = yajl.to_value(body)
     if npb.qws then
       npb.qws = false
-      npb.qwsorted(data)
+      local people = {}
+      for _, person in pairs(data.characters) do
+        table.insert(people, person.name)
+      end
+      npb.qwsorted(people)
     end
   end
 end
 
 registerAnonymousEventHandler("sysGetHttpDone", "npb.handleResponse")
 
-local order = {"Bloodloch", "Duiran", "Enorian", "Spinesreach", "Rogues", "Divine"}
+local order = { "Bloodloch", "Duiran", "Enorian", "Spinesreach", "Rogues", "Divine" }
 local function newLine()
   moveCursorEnd()
   if getCurrentLine() ~= "" then
@@ -27,17 +31,16 @@ function npb.cformat(player, format)
   local str = "<reset>"
   local color = sndNDB_getColour(player)
   if color then
-    str = str.."<"..color..">"
+    str = str .. "<" .. color .. ">"
   end
-  str = str..player:lower():title().."<reset>"..(format and format or "")
+  str = str .. player:lower():title() .. "<reset>" .. (format and format or "")
   return str
 end
 
 function npb.qwsorted(people)
   local wrap = getWindowWrap()
-  local sorted = {Bloodloch = {}, Duiran = {}, Enorian = {}, Spinesreach = {}, Rogues = {}, Divine = {}}
-  for _, person in pairs(people.characters) do
-    local name = person.name
+  local sorted = { Bloodloch = {}, Duiran = {}, Enorian = {}, Spinesreach = {}, Rogues = {}, Divine = {} }
+  for _, name in pairs(people) do
     local org = sndNDB_getCitizenship(name)
     if org == "Unknown" then org = "Rogues" end
     table.insert(sorted[org], name)
@@ -45,16 +48,18 @@ function npb.qwsorted(people)
   for _, org in ipairs(order) do
     local peoples = sorted[org]
     table.sort(peoples)
-    if #peoples>0 then
+    if #peoples > 0 then
       newLine()
-      cecho("<"..npb.t.config.colors[org:lower()]..">"..
-      string.rep(" ", 11-#org).."<b><u>"..org:title().."<reset>"..(#peoples>9 and "" or " ").."<white>(<cyan>"..#peoples.."<white>): ")
+      cecho("<" .. npb.t.config.colors[org:lower()] .. ">" ..
+        string.rep(" ", 11 - #org) ..
+        "<b><u>" .. org:title() .. "<reset>" .. (#peoples > 9 and "" or " ") .. "<white>(<cyan>" ..
+        #peoples .. "<white>): ")
       for i, v in pairs(peoples) do
         local linelength = #getCurrentLine()
         local comma = next(peoples, i)
         local personlength = (comma and 2 or 0) + #v
-        if linelength+personlength>wrap then
-          echo("\n"..string.rep(" ",17))
+        if linelength + personlength > wrap then
+          echo("\n" .. string.rep(" ", 17))
           moveCursorEnd()
         end
         cecho(npb.cformat(v))
@@ -65,3 +70,4 @@ function npb.qwsorted(people)
   echo("\n")
   moveCursorEnd()
 end
+
