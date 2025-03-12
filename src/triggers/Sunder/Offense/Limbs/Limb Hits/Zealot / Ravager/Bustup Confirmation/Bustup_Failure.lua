fd897722dd4da9c163f20ed656c22dd907e0a184
TheWanderingCrow <contact@wanderingcrow.net>
2025-03-12 15:46:44 -0400

    Sunder 7.0.7 Removed some old triggers that weren't used

--- src/triggers/Sunder/Offense/Limbs/Limb Hits/Zealot / Ravager/Bustup Confirmation/Bustup_Failure.lua
+++
@@ -1,1 +0,0 @@
-disableTrigger("Bustup Confirmation")
--- src/triggers/Sunder/Offense/Limbs/Limb Hits/Zealot / Ravager/Bustup Confirmation/Bustup_Success.lua
+++
@@ -1,7 +0,0 @@
-local limb = ""
-limb = multimatches[1][3]:gsub(" ", "_")
-
-if multimatches[1][2] == snd.target then
-  snd.target_got(multimatches[1][3].."_dislocated")
-end
-disableTrigger("Bustup Confirmation")
