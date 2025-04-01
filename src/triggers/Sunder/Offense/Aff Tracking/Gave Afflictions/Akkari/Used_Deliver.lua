4a9fc0390cc05575debc231e9ee7f2e38bab0591
TheWanderingCrow <contact@wanderingcrow.net>
2025-04-01 09:06:48 -0400

    Final deletythings pass

--- src/triggers/Sunder/Offense/Aff Tracking/Gave Afflictions/Akkari/Used_Deliver.lua
+++
@@ -1,4 +0,0 @@
-if matches[2] == snd.target then
-  snd.target_got("feed")
-  feed_timer = tempTimer(10, [[snd.target_cured("feed")]])
-end
--- src/triggers/Sunder/Offense/Aff Tracking/Gave Afflictions/Monk/Telepathy/mind_blank.lua
+++
@@ -1,4 +0,0 @@
-if snd.target == matches[2] then
-	snd.target_got("turmoil")
-  tempTimer(60, [[snd.target_cured("turmoil")]])
-end
