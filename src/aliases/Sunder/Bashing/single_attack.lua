dab0f29f2a2e3b5af446aa000cd3f84bc8bda0ec
Joshua Marantz <marantzj@gmail.com>
2025-04-02 01:03:40 +0000

    fix single attack alias for new parrying

--- src/aliases/Sunder/Bashing/single_attack.lua
+++ src/aliases/Sunder/Bashing/single_attack.lua
@@ -8,11 +8,15 @@ elseif snd[class .. "_bash"] then
 else
   battack = "kill " .. snd.target                       --if no bashing function exists, default to kill
 end
+local commands = [battack]
 if snd.have_aff("shock") and hasSkill("Overdrive") then -- let's use overdrive if we have shock, adds to new class bashing attack line
-  battack = "overdrive" .. snd.sep .. battack .. snd.sep
+  table.insert(commands, 1, "overdrive")
 end
-if snd.toggles.parrying and snd.parrying.current_parry.toparry ~= "none" and snd.parrying.toparry ~= snd.parrying.current_parry.current_parry then
-  battack = "parry " .. snd.parrying.current_parry.toparry .. snd.sep .. battack
+if snd.toggles.parrying and snd.parrying.current_parry ~= "none" and snd.parrying.toparry ~= snd.parrying.current_parry then
+  table.insert(commands, 1, "parry " .. snd.parrying.toparry)
 end
-if tonumber(snd.toggles.gauntlet_level) >= 2 then battack = battack .. snd.sep .. "absorb ylem" end
-snd.send("qeb stand" .. snd.sep .. battack)
+if tonumber(snd.toggles.gauntlet_level) >= 2 then
+  table.insert(commands, "absorb ylem")
+end
+table.insert(commands, 1, "qeb stand")
+snd.send(table.concat(commands, snd.sep))
