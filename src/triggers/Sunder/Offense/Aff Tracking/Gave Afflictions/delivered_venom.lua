2349b6f1f5e0fcd08dbc72f4224833e0f043fccc
TheWanderingCrow <contact@wanderingcrow.net>
2025-04-07 17:49:38 -0400

    fix vayua strip rebounding

--- src/triggers/Sunder/Offense/Aff Tracking/Gave Afflictions/delivered_venom.lua
+++ src/triggers/Sunder/Offense/Aff Tracking/Gave Afflictions/delivered_venom.lua
@@ -1,15 +1,27 @@
 if snd.getting_hit ~= "You" then
-  if snd.toggles.atkecho and snd.toggles.gags then 
-   deleteLine()
-   snd.message("Gave: "..matches[2])
+  if snd.toggles.atkecho and snd.toggles.gags then
+    deleteLine()
+    snd.message("Gave: " .. matches[2])
   end
-  
-  local attacks = {Backstab = true, Lob = true, Assault = true, Slaughter = true, Carve = true, Cripple = true, Bloodscourge = true,
-                   Fleshbane = true, Butterfly = true, Athame = true, Bonedagger = true, }
+
+  local attacks = {
+    Backstab = true,
+    Lob = true,
+    Assault = true,
+    Slaughter = true,
+    Carve = true,
+    Cripple = true,
+    Bloodscourge = true,
+    Fleshbane = true,
+    Butterfly = true,
+    Athame = true,
+    Bonedagger = true,
+    Vayua = true,
+  }
   if attacks[snd.skill_being_used] == nil then
     snd.used.rebounding = false
   end
-   
+
   snd.last_affs = {}
   local aff = snd.venomEffect(matches[2])
   snd.target_got(aff)
