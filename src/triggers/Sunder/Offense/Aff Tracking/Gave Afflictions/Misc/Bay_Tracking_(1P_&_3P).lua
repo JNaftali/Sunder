37e3c7d88193caedfdd3e68911ab57b313fedad2
TheWanderingCrow <contact@wanderingcrow.net>
2025-04-10 08:31:29 -0400

    Sunder 10.1.1
    fix knitting aliviate done
    
    shifter: blurry to watery_eyes
    
    Batter call firelength set to 0
    
    New mining targets

--- src/triggers/Sunder/Offense/Aff Tracking/Gave Afflictions/Misc/Bay_Tracking_(1P_&_3P).lua
+++ src/triggers/Sunder/Offense/Aff Tracking/Gave Afflictions/Misc/Bay_Tracking_(1P_&_3P).lua
@@ -1,30 +1,30 @@
 if multimatches[2][2] == "wailing" then return end
 if multimatches[2][2] == "failure" then return end
 local howl_conversion =
-  {
-    ["piercing"] = "hearing",
-    ["paralyzing"] = "paresis",
-    ["baleful"] = "impairment",
-    ["rousing"] = "recklessness",
-    ["distasteful"] = "anorexia",
-    ["forceful"] = "fallen",
-    ["mind-numbing"] = "stupidity",
-    ["stomach-turning"] = "vomiting",
-    ["claustrophobic"] = "claustrophobia",
-    ["muddling"] = "peace",
-    ["hypnotic"] = "hypersomnia",
-    ["deranged"] = "hallucinations",
-    ["lethargic"] = "lethargy",
-    ["ringing"] = "ringing_ears",
-    ["blurring"] = "blurry_vision",
-    ["debilitating"] = "weariness",
-    ["mania"] = "mania",
-    ["angry"] = "hatred",
-    --["disturbing"] = "disrupted",
-    ["befuddling"] = "confusion",
-    ["deep"] = "plodding",
-    ["dumbing"] = "idiocy",
-  }
+{
+  ["piercing"] = "hearing",
+  ["paralyzing"] = "paresis",
+  ["baleful"] = "impairment",
+  ["rousing"] = "recklessness",
+  ["distasteful"] = "anorexia",
+  ["forceful"] = "fallen",
+  ["mind-numbing"] = "stupidity",
+  ["stomach-turning"] = "vomiting",
+  ["claustrophobic"] = "claustrophobia",
+  ["muddling"] = "peace",
+  ["hypnotic"] = "hypersomnia",
+  ["deranged"] = "hallucinations",
+  ["lethargic"] = "lethargy",
+  ["ringing"] = "ringing_ears",
+  ["blurring"] = "watery_eyes",
+  ["debilitating"] = "weariness",
+  ["mania"] = "mania",
+  ["angry"] = "hatred",
+  --["disturbing"] = "disrupted",
+  ["befuddling"] = "confusion",
+  ["deep"] = "plodding",
+  ["dumbing"] = "idiocy",
+}
 
 if multimatches[2][3] == snd.target and howl_conversion[multimatches[2][2]] ~= nil then
   snd.target_got(howl_conversion[multimatches[2][2]])
